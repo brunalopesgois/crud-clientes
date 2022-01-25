d7b7fa44dac50a9ab2f12123cdcdd6197d5bf8df
Bruna L Gois <bruna-chan@live.com>
2022-01-25 16:57:37 -0300

    feat: client controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { ClientsController } from './clients.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ClientsController],
   providers: [AppService],
 })
 export class AppModule {}
