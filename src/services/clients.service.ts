082b6419cb9194b4d3a4cf3dea5242fdb79366ec
Bruna L Gois <bruna-chan@live.com>
2022-01-26 15:54:58 -0300

    feat: encrypt password

--- src/services/clients.service.ts
+++ src/services/clients.service.ts
@@ -2,6 +2,7 @@ import { Client } from 'src/entities/client.entity';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { EntityRepository } from '@mikro-orm/mysql';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class ClientsService {
@@ -23,11 +24,13 @@ export class ClientsService {
       email: client.email,
     });
 
+    const password = await this.hashPassword(client.password);
+
     if (!alreadyExists) {
       const newClient = new Client(
         client.alias,
         client.email,
-        client.password,
+        password,
         client.phone,
       );
       this.clientRepository.persistAndFlush(newClient);
@@ -36,10 +39,13 @@ export class ClientsService {
 
   async update(id: number, client: Client): Promise<Client> {
     const existentClient = await this.findOne(id);
+
+    const password = await this.hashPassword(client.password);
+
     const newClient = this.clientRepository.assign(existentClient, {
       alias: client.alias,
       email: client.email,
-      password: client.password,
+      password: password,
       phone: client.phone,
     });
     this.clientRepository.persistAndFlush(newClient);
@@ -50,5 +56,9 @@ export class ClientsService {
   async delete(id: number) {
     const client = await this.findOne(id);
     this.clientRepository.removeAndFlush(client);
+  }
+
+  private async hashPassword(text: string): Promise<string> {
+    return await bcrypt.hash(text, 10);
   }
 }
