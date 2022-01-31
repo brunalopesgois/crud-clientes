f4b0b700aef417ffded6474472d74f93753d75af
Bruna L Gois <bruna-chan@live.com>
2022-01-31 16:37:27 -0300

    refactor: improve exception handling on client

--- src/services/clients.service.ts
+++ src/services/clients.service.ts
@@ -1,14 +1,16 @@
 import { Client } from 'src/entities/client.entity';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { EntityRepository } from '@mikro-orm/mysql';
 import hasher from 'src/utils/passwordHasher';
+import { EntityManager } from '@mikro-orm/core';
 
 @Injectable()
 export class ClientsService {
   constructor(
     @InjectRepository(Client)
     private readonly clientRepository: EntityRepository<Client>,
+    private readonly em: EntityManager,
   ) {}
 
   async findAll(): Promise<Client[]> {
@@ -24,22 +26,41 @@ export class ClientsService {
       email: client.email,
     });
 
+    if (alreadyExists) {
+      throw new HttpException(
+        `The client with email ${client.email} alread exists`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     const password = await hasher.hashPassword(client.password);
 
-    if (!alreadyExists) {
-      const newClient = new Client(
-        client.tax_id,
-        client.alias,
-        client.email,
-        password,
-        client.phone,
-      );
-      this.clientRepository.persistAndFlush(newClient);
+    const newClient = new Client(
+      client.tax_id,
+      client.alias,
+      client.email,
+      password,
+      client.phone,
+    );
+    this.em.begin();
+    try {
+      await this.clientRepository.persistAndFlush(newClient);
+      this.em.commit();
+    } catch (error) {
+      this.em.rollback();
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
   async update(id: number, client): Promise<Client> {
     const existentClient = await this.findById(id);
+
+    if (!existentClient) {
+      throw new HttpException(
+        `The client with id ${id} does not exist`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
 
     const password = await hasher.hashPassword(client.password);
 
@@ -50,13 +71,27 @@ export class ClientsService {
       password: password,
       phone: client.phone,
     });
-    this.clientRepository.persistAndFlush(newClient);
+    this.em.begin();
+    try {
+      this.clientRepository.persistAndFlush(newClient);
+      this.em.commit();
+    } catch (error) {
+      this.em.rollback();
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     return newClient;
   }
 
   async delete(id: number) {
     const client = await this.findById(id);
-    this.clientRepository.removeAndFlush(client);
+    this.em.begin();
+    try {
+      await this.clientRepository.removeAndFlush(client);
+      this.em.commit();
+    } catch (error) {
+      this.em.rollback();
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
