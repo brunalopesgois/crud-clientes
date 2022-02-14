796f002d6e3b2b27a222b8f7cfba027b84293adb
Bruna L Gois <bruna-chan@live.com>
2022-02-14 16:27:26 -0300

    feat: update mikro-orm for use seeders

---
+++ migrations/Migration20220214192652.ts
@@ -0,0 +1,12 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20220214192652 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('create table "wallet" ("id" serial primary key, "client_id" int not null, "alias" varchar(255) null, "status" text check ("status" in (\'created\', \'active\', \'inactive\', \'finished\')) not null, "balance" int null default 0, "ispb" varchar(255) not null, "bank_branch" varchar(255) not null, "bank_number" varchar(255) not null, "created_at" date null, "updated_at" date null);');
+
+    this.addSql('create table "client" ("id" serial primary key, "tax_id" varchar(255) not null, "alias" varchar(255) not null, "email" varchar(255) not null, "password" varchar(255) not null, "phone" varchar(255) not null, "created_at" date null, "updated_at" date null);');
+    this.addSql('alter table "client" add constraint "client_email_unique" unique ("email");');
+  }
+
+}
